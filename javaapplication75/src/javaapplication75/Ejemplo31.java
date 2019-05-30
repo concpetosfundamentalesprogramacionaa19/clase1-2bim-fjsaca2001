26eb3ae15170cb9191c1412efd70899b8b11bc55
fjsaca2001 <sacafrank2@gmail.com>
2019-05-30 11:48:46 -0500

    Trabajo en clase

--- javaapplication75/src/javaapplication75/Ejemplo31.java
+++ javaapplication75/src/javaapplication75/Ejemplo31.java
@@ -23,48 +23,48 @@ public class Ejemplo31 {
         // Random = valores aleatorios
         Random aleatorios = new Random();
         int[] datos = new int[6];
-        String[] rangos = {"0-8", "8.1-12", "12.1-16","16.1-19","19.1-20","20.1-adelante"};
+        String[] rangos = {"0-8", "8.1-12", "12.1-16", "16.1-19", "19.1-20", "20.1-adelante"};
         // Ciclo while para que el usuario decida cuando terminar
-        while(b == true){
-        System.out.printf("Ingrese una nota: ");
-        double cantidad = entrada.nextDouble();
-        // asignacion de datos al arreglo segun corresponde
-        if(cantidad >= 0 && cantidad <= 8){
-            datos[0] = datos[0] + 1;
-        }else{
-            if(cantidad >=8.1 && cantidad <= 12){
-                datos[1] = datos[1] + 1;
-            }else{
-                if(cantidad >=12.1 && cantidad <= 16){
-                    datos[2] = datos[2] + 1;
-                }else{
-                    if(cantidad >=16.1 && cantidad <= 19){
-                        datos[3] = datos[3] + 1;
-                    }else{
-                            if(cantidad >=19.1 && cantidad <= 20){
-                                 datos[4] = datos[4] + 1;   
-                            }else{
+        while (b == true) {
+            System.out.printf("Ingrese una nota: ");
+            double cantidad = entrada.nextDouble();
+            // asignacion de datos al arreglo segun corresponde
+            if (cantidad >= 0 && cantidad <= 8) {
+                datos[0] = datos[0] + 1;
+            } else {
+                if (cantidad >= 8.1 && cantidad <= 12) {
+                    datos[1] = datos[1] + 1;
+                } else {
+                    if (cantidad >= 12.1 && cantidad <= 16) {
+                        datos[2] = datos[2] + 1;
+                    } else {
+                        if (cantidad >= 16.1 && cantidad <= 19) {
+                            datos[3] = datos[3] + 1;
+                        } else {
+                            if (cantidad >= 19.1 && cantidad <= 20) {
+                                datos[4] = datos[4] + 1;
+                            } else {
                                 datos[5] = datos[5] + 1;
-                         }
+                            }
+                        }
                     }
                 }
             }
+            // limpieza del Buffer
+            entrada.nextLine();
+            System.out.printf("Presione s para agregar otra nota: ");
+            a = entrada.nextLine();
+            // validacion de si se quiere o no agregar mas notas
+            if (a.equals("s")) {
+                b = true;
+            } else {
+                b = false;
+            }
         }
-        // limpieza del Buffer
-        entrada.nextLine();
-        System.out.printf("Presione s para agregar otra nota: ");
-        a = entrada.nextLine();
-        // validacion de si se quiere o no agregar mas notas
-        if(a.equals("s")){
-            b = true;
-        }else{
-            b = false;
-        }
-       }
         // presentacion de datos
-        System.out.println("Notas ingresadas...\n"); 
-        for(int i = 0; i<datos.length; i++){
-            System.out.printf("Notas en %s - %d\n", rangos[i], datos[i]);
+        System.out.println("Notas ingresadas...\nRangos: ");
+        for (int i = 0; i < datos.length; i++) {
+            System.out.printf("%s - %d\n", rangos[i], datos[i]);
         }
     }
 }
